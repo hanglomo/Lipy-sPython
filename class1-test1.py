83eff291e68dba5bf340065a80926bd58a9b404a
hanglomo <shhlomo@Outlook.com>
2021-04-30 09:01:51 +0800

    Create class1-test1.py
    李芃宇第一节Python课堂源码

---
+++ class1-test1.py
@@ -0,0 +1,16 @@
+    print("Hello Word!")
+    print("我是李芃宇。")
+    print("   *")
+    print("  ***")
+    print(" *****")
+    print("*******")
+    H=1
+    print(H)
+
+
+    #计算年龄和
+    H=int(input("请输入海绵宝宝年龄："))
+    E=int(input("请输入派大星年龄："))
+    D=H+E
+    print("派大星和海绵宝宝年龄和是：",D)
+    
